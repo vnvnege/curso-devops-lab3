5401610fe5413043364bf9e40b8c0efbcb884613
vnvnege <vnvenega@gmail.com>
2026-04-23 11:45:26 -0400

    agregadas pruebas via REST

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsModule } from './items/items.module';
-import { HealthModule } from './health/health.module';
+//import { ItemsModule } from './items/items.module';
+//import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ItemsModule, HealthModule],
+  //imports: [ItemsModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
