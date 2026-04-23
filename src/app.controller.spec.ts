74555ea852cd362942397f5a7f5c0be1e9d13d35
vnvnege <vnvenega@gmail.com>
2026-04-23 11:12:33 -0400

    complejizando el test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { INestApplication } from '@nestjs/common';
+import { App } from 'supertest/types';
+import { AppModule } from './app.module';
+import request from 'supertest';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -17,10 +21,38 @@ describe('AppController', () => {
   describe('Probando la raiz del sitio', () => {
     it('probando funcion getHello"', () => {
       expect(appController.getHello()).toBe('Hello World!');
-      expect(appController.getHello()).not.toBe('Hola Mundo!');
+      expect(appController.getHello()).not.toBe('Hi there!!!');
     });
     it('probando funcion getSaludo"', () => {
       expect(appController.getSaludo()).toBe('Hola Mundo!');
     });
   });
 });
+
+describe('AppController (Via Rest)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET', () => {
+    return request(app.getHttpServer())
+      .get('/hello')
+      .expect(200)
+      .expect('Hello World!');
+  });
+
+  it('/GET', () => {
+    return request(app.getHttpServer())
+      .get('/saludo')
+      .expect(200)
+      .expect('Hola Mundo!');
+  });
+});
+
