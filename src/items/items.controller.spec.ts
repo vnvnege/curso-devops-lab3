afa4d37786c9e2e385ba88ceb1eeb349ab7d8add
vnvnege <vnvenega@gmail.com>
2026-04-24 22:36:52 -0400

    agregando test a items

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,8 +1,8 @@
-import { INestApplication } from '@nestjs/common';
+//import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import request from 'supertest';
-import { App } from 'supertest/types';
-import { ItemsModule } from './items.module';
+//import request from 'supertest';
+//import { App } from 'supertest/types';
+//import { ItemsModule } from './items.module';
 import { ItemsService } from './items.service';
 
 describe('ItemsService', () => {
