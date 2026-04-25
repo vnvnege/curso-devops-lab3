38028d9ac6f49624d11b9040f95a0698851de78e
vnvnege <vnvenega@gmail.com>
2026-04-25 08:02:54 -0400

    corrigiendo app modulo agregando healt e items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { ItemsModule } from './items/items.module';
-//import { HealthModule } from './health/health.module';
+import { ItemsModule } from './items/items.module';
+import { HealthModule } from './health/health.module';
 import { CalculoModule } from './operacion/calculo/calculo.module';
 
 @Module({
-  //imports: [ItemsModule, HealthModule],
+  imports: [ItemsModule, HealthModule, CalculoModule],
   controllers: [AppController],
   providers: [AppService],
-  imports: [CalculoModule],
+  //imports: [CalculoModule],
 })
 export class AppModule {}
