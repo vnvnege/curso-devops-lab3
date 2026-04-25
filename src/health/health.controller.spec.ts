ff6d84238258d9054a13da513177a2c87e192c10
vnvnege <vnvenega@gmail.com>
2026-04-24 21:28:56 -0400

    agregando test a health

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -3,8 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { HealthModule } from './health.module';
-
-
+import { HealthService } from './health.service';
 
 describe('HealthController', () => {
   let app: INestApplication<App>;
@@ -22,9 +21,42 @@ describe('HealthController', () => {
     await app.close();
   });
 
-  test('GET / Tiempo transcurrido', () => {    
+  test('GET / No carga pagina', () => {    
     return request(app.getHttpServer())
-      .get('')
-      .expect(404);
+      .get('/')
+      .expect(404)
+      .expect({status: 'ok'});
   });
 });
+
+/*
+describe('HealthService', () => {
+  let service: HealthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [HealthService],
+    }).compile();
+
+    service = module.get<HealthService>(HealthService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  test('Deberia mostrar resultado', () => {
+    expect(service.operar('suma', 10, 50)).toBe(60);
+
+    let a = 10;
+    let b = 30;
+
+    expect(service.operar('suma', a, b)).not.toBe(41);
+
+    a = -100;
+    b = 10;
+
+    expect(service.operar('suma', a, b)).toBe(-90);
+  });
+})
+  */
