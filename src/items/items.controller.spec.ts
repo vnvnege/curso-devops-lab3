b39a0f690b790a527d8dfd1deda36648820b0330
vnvnege <vnvenega@gmail.com>
2026-04-25 00:04:11 -0400

    agregando test a items

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -26,7 +26,9 @@ describe('ItemsService', () => {
 
   test('encontrar uno', () => {
     expect(service.findOne(1)).toHaveProperty('name', 'Item uno');
-    expect(service.findOne(3)).toThrow('Item 3 no encontrado',);
+    expect(() =>
+      service.findOne(3))
+      .toThrow('Item 3 no encontrado',);
   });
 
   test('agregar caso 3', () => {
@@ -35,7 +37,9 @@ describe('ItemsService', () => {
 
   test('borrar caso 3', () => {
     expect(service.remove(3)).toBeNull();
-    expect(service.findOne(3)).toThrow('Item 3 no encontrado',);
+    expect(() =>
+      service.findOne(3))
+      .toThrow('Item 3 no encontrado',);
   });
 
 });
