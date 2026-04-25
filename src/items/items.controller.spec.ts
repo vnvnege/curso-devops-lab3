e57341d1939c92d0a7a3a07e6fd6ea54af072b39
vnvnege <vnvenega@gmail.com>
2026-04-24 22:34:38 -0400

    agregando test a items

---
+++ src/items/items.controller.spec.ts
@@ -0,0 +1,69 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { ItemsModule } from './items.module';
+import { ItemsService } from './items.service';
+
+describe('ItemsService', () => {
+  let service: ItemsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ItemsService],
+    }).compile();
+
+    service = module.get<ItemsService>(ItemsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  test('encontrar todos', () => {
+    expect(service.findAll()).toHaveProperty('id');
+    expect(service.findAll()).toHaveProperty('name');
+    expect(service.findAll()).toHaveProperty('description');    
+  });
+
+  test('encontrar el primer valor', () => {
+    expect(service.findOne(1)).toHaveProperty('name', 'Item uno');
+  });
+
+  test('creando registro 3', () => {
+    expect(service.create('el item tres', 'tercer valor')).toHaveReturned();
+    expect(service.findOne(3)).toHaveProperty('name', 'el item tres');
+  });
+
+  test('borrando registro 3', () => {
+    expect(service.remove(3)).toHaveReturned();
+    expect(service.findOne(3)).toBeNaN();
+  });
+})
+
+/*
+
+describe('ItemsController', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [ItemsModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  test('GET / Pagina que no abre', () => {    
+    return request(app.getHttpServer())
+      .get('')
+      .expect(404);
+  });
+});
+
+*/
